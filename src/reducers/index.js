330a497b04da6e67566bc7f1a6b3986ea6c7c591
Eugene Obrezkov <ghaiklor@gmail.com>
2017-05-18 13:32:56 +0300

    Implement PoC for running a color

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,36 @@
 import * as TYPES from '../actions/types';
 
+function getAdjacents(array, index) {
+  const size = Math.sqrt(array.length);
+  const top = index - size;
+  const right = index + 1;
+  const bottom = index + size;
+  const left = index - 1;
+
+  return {top, right, bottom, left};
+}
+
 export default function floodIt(state, action) {
   switch (action.type) {
     case TYPES.RUN_COLOR:
-      const color = action.color;
-      console.log(color);
-      return state;
+      const field = [].concat(state.field);
+      const oldColor = field[0];
+      const newColor = action.color;
+      const job = [0];
+
+      while (job.length) {
+        const curIndex = job.pop();
+        const {top, right, bottom, left} = getAdjacents(field, curIndex);
+
+        field[curIndex] = newColor;
+
+        if (field[top] === oldColor) job.push(top);
+        if (field[right] === oldColor) job.push(right);
+        if (field[bottom] === oldColor) job.push(bottom);
+        if (field[left] === oldColor) job.push(left);
+      }
+
+      return Object.assign({}, state, {field, currentColor: newColor});
     default:
       return state;
   }
