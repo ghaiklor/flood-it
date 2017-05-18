9b232930be512cbe3237f7917980df53f91f6ad0
Eugene Obrezkov <ghaiklor@gmail.com>
2017-05-18 19:19:12 +0300

    style: improve style in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import * as TYPES from '../actions/types';
 import getAdjacentIndexes from '../common/getAdjacentIndexes';
 import generateNewField from '../common/generateNewField';
 
-const INITIAL_FIELD_SIZE = 14;
+const INITIAL_FIELD_SIZE = 10;
 const INITIAL_COLORS_COUNT = 5;
 const initialField = generateNewField(INITIAL_FIELD_SIZE, INITIAL_COLORS_COUNT);
 const initialState = {
@@ -38,7 +38,7 @@ export default function (state = initialState, action) {
     case TYPES.NEW_GAME: {
       const field = generateNewField(state.fieldSize, state.colorsCount);
 
-      return Object.assign({}, state, {spentMoves: 0, currentColorIndex: field[0], field: field});
+      return Object.assign({}, state, {spentMoves: 0, currentColorIndex: field[0], field});
     }
     default:
       return state;
