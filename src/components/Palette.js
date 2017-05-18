c35df8da1e0067fc44a7bcc7c9b2830bc48a145b
Eugene Obrezkov <ghaiklor@gmail.com>
2017-05-18 10:10:42 +0300

    Implement binding selected color to onPress in Palette

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -5,13 +5,18 @@ import Tile from '../components/Tile';
 const {width: WIDTH} = Dimensions.get('window');
 
 export default class Palette extends React.Component {
-  _onPress() {
+  _onPress(color) {
+    console.log(color);
+  }
+
+  createTile(size, color, i) {
+    return <Tile key={i} color={color} size={size} onPress={this._onPress.bind(this, color)}/>;
   }
 
   render() {
     const {colors} = this.props;
     const tileSize = WIDTH / colors.length;
-    const tiles = colors.map((color, i) => <Tile key={i} color={color} size={tileSize} onPress={this._onPress}/>);
+    const tiles = colors.map(this.createTile.bind(this, tileSize));
 
     return <View style={{flexDirection: 'row'}}>{tiles}</View>;
   }
