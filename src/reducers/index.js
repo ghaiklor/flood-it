aa1f84fdb58f43d64124232bd68745d4bd8e3262
Eugene Obrezkov <ghaiklor@gmail.com>
2017-05-19 18:04:23 +0300

    style: improve style in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import * as TYPES from '../actions/types';
+import calculateMaxMoves from '../common/calculateMaxMoves';
 import getAdjacentIndexes from '../common/getAdjacentIndexes';
 import generateNewField from '../common/generateNewField';
 
@@ -9,28 +10,29 @@ const MAXIMUM_FIELD_SIZE = 26;
 
 const initialField = generateNewField(MINIMUM_FIELD_SIZE, MINIMUM_COLORS_COUNT);
 const initialState = {
+  currentLevel: 1,
   fieldSize: MINIMUM_FIELD_SIZE,
   colorsCount: MINIMUM_COLORS_COUNT,
-  level: 1,
-  spentMoves: 0,
-  done: 0,
+  field: initialField,
   currentColorIndex: initialField[0],
-  field: initialField
+  maxMoves: calculateMaxMoves(MINIMUM_FIELD_SIZE, MINIMUM_COLORS_COUNT),
+  spentMoves: 0,
+  done: 0
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
     case TYPES.RUN_COLOR: {
-      const field = [].concat(state.field);
+      const field = state.field.slice(0);
       const oldColor = field[0];
       const newColor = action.color;
       const job = [0];
 
       while (job.length) {
-        const curIndex = job.pop();
-        const {top, right, bottom, left} = getAdjacentIndexes(field.length, curIndex);
+        const index = job.pop();
+        const {top, right, bottom, left} = getAdjacentIndexes(field.length, index);
 
-        field[curIndex] = newColor;
+        field[index] = newColor;
 
         if (field[top] === oldColor) job.push(top);
         if (field[right] === oldColor) job.push(right);
@@ -38,52 +40,41 @@ export default function (state = initialState, action) {
         if (field[left] === oldColor) job.push(left);
       }
 
+      const spentMoves = state.spentMoves + 1;
       const done = Math.ceil(field.filter(colorIndex => colorIndex === newColor).length * 100 / field.length);
 
-      return Object.assign({}, state, {
-        spentMoves: state.spentMoves + 1,
-        done: done,
-        currentColorIndex: newColor,
-        field
-      });
+      return {...state, field, currentColorIndex: newColor, spentMoves, done};
     }
 
     case TYPES.NEW_GAME: {
-      const field = generateNewField(state.fieldSize, state.colorsCount);
+      const {fieldSize, colorsCount} = state;
+      const field = generateNewField(fieldSize, colorsCount);
+      const currentColorIndex = field[0];
+      const maxMoves = calculateMaxMoves(fieldSize, colorsCount);
+      const spentMoves = 0;
+      const done = 0;
 
-      return Object.assign({}, state, {spentMoves: 0, done: 0, currentColorIndex: field[0], field});
+      return {...state, field, currentColorIndex, maxMoves, spentMoves, done};
     }
 
     case TYPES.INCREASE_DIFFICULTY: {
-      let fieldSize = state.fieldSize;
-      let colorsCount = state.colorsCount;
+      if (state.fieldSize === MAXIMUM_FIELD_SIZE && state.colorsCount === MAXIMUM_COLORS_COUNT) return state;
 
-      if (fieldSize === MAXIMUM_FIELD_SIZE && colorsCount === MAXIMUM_COLORS_COUNT) return state;
+      const currentLevel = state.currentLevel + 1;
+      const fieldSize = state.fieldSize === MAXIMUM_FIELD_SIZE ? MINIMUM_FIELD_SIZE : state.fieldSize + 1;
+      const colorsCount = state.fieldSize === MAXIMUM_FIELD_SIZE ? state.colorsCount + 1 : state.colorsCount;
 
-      if (fieldSize === MAXIMUM_FIELD_SIZE) {
-        fieldSize = MINIMUM_FIELD_SIZE;
-        colorsCount = Math.min(MAXIMUM_COLORS_COUNT, colorsCount + 1);
-      } else {
-        fieldSize += 1;
-      }
-
-      return Object.assign({}, state, {level: state.level + 1, fieldSize, colorsCount});
+      return {...state, currentLevel, fieldSize, colorsCount};
     }
 
     case TYPES.DECREASE_DIFFICULTY: {
-      let fieldSize = state.fieldSize;
-      let colorsCount = state.colorsCount;
+      if (state.fieldSize === MINIMUM_FIELD_SIZE && state.colorsCount === MINIMUM_COLORS_COUNT) return state;
 
-      if (fieldSize === MINIMUM_FIELD_SIZE && colorsCount === MINIMUM_COLORS_COUNT) return state;
+      const currentLevel = state.currentLevel - 1;
+      const fieldSize = state.fieldSize === MINIMUM_FIELD_SIZE ? MAXIMUM_FIELD_SIZE : state.fieldSize - 1;
+      const colorsCount = state.fieldSize === MINIMUM_FIELD_SIZE ? state.colorsCount - 1 : state.colorsCount;
 
-      if (fieldSize === MINIMUM_FIELD_SIZE) {
-        fieldSize = MAXIMUM_FIELD_SIZE;
-        colorsCount = Math.max(MINIMUM_COLORS_COUNT, colorsCount - 1);
-      } else {
-        fieldSize -= 1;
-      }
-
-      return Object.assign({}, state, {level: state.level - 1, fieldSize, colorsCount});
+      return {...state, currentLevel, fieldSize, colorsCount};
     }
 
     default:
