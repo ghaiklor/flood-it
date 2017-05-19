0fca384d263b904fd7b93a5a4744ece420c7e47f
Eugene Obrezkov <ghaiklor@gmail.com>
2017-05-19 13:09:03 +0300

    feature: implement persistent storage for getting back to last place

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import Expo from 'expo';
 import React from 'react';
+import {AsyncStorage} from 'react-native';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {compose, applyMiddleware, createStore} from 'redux';
+import {persistStore, autoRehydrate} from 'redux-persist';
 import reducer from './src/reducers';
 import App from './src/App';
 
-const store = createStore(reducer);
+const store = createStore(reducer, undefined, compose(applyMiddleware(), autoRehydrate()));
+persistStore(store, {storage: AsyncStorage});
 
 class FloodIt extends React.Component {
   render() {
