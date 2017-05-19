7c6104b979e467e588150d58644a9a5dd63321b4
Eugene Obrezkov <ghaiklor@gmail.com>
2017-05-19 20:36:28 +0300

    test: fix tests for max moves

--- __tests__/common/calculateMaxMoves.test.js
+++ __tests__/common/calculateMaxMoves.test.js
@@ -2,10 +2,10 @@ import {assert} from 'chai';
 import calculateMaxMoves from '../../src/common/calculateMaxMoves';
 
 it('Should properly calculate maximum moves in a game', () => {
-  assert.equal(calculateMaxMoves(6, 3), 5);
-  assert.equal(calculateMaxMoves(7, 3), 6);
-  assert.equal(calculateMaxMoves(8, 4), 10);
-  assert.equal(calculateMaxMoves(9, 5), 14);
-  assert.equal(calculateMaxMoves(10, 6), 19);
-  assert.equal(calculateMaxMoves(20, 8), 51);
+  assert.equal(calculateMaxMoves(6, 3), 6);
+  assert.equal(calculateMaxMoves(7, 3), 7);
+  assert.equal(calculateMaxMoves(8, 4), 11);
+  assert.equal(calculateMaxMoves(9, 5), 16);
+  assert.equal(calculateMaxMoves(10, 6), 21);
+  assert.equal(calculateMaxMoves(20, 8), 57);
 });
